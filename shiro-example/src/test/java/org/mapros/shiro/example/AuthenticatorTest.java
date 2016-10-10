feeb966b69f786d285898470797258c6e671120a
mapros <mapros@sina.com>
2016-10-10 17:21:22 +0800

    jdbc realm ddl and ini configuration

---
+++ shiro-example/src/test/java/org/mapros/shiro/example/AuthenticatorTest.java
@@ -0,0 +1,54 @@
+package org.mapros.shiro.example;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.Factory;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
+/**
+ * Created by HP on 2016/10/10.
+ *
+ * @author mapros
+ */
+public class AuthenticatorTest {
+    @Test
+    public void testAllSuccessfulStrategy() throws Exception {
+        doLogin("classpath:shiro.authenticator.all.success.ini", "mapros", "984138");
+        Subject subject = SecurityUtils.getSubject();
+        PrincipalCollection principals = subject.getPrincipals();
+        Assert.assertEquals(2, principals.asList().size());
+    }
+
+    @Test(expected = org.apache.shiro.authc.UnknownAccountException.class)
+    public void testAllFailedStrategy() throws Exception {
+        doLogin("classpath:shiro.authenticator.all.failed.ini", "mapros", "984138");
+    }
+
+    @Test
+    public void testFirstSuccessfulStrategy() throws Exception {
+        doLogin("classpath:shiro.authenticator.first.success.ini", "mapros", "984138");
+        Assert.assertEquals(1,SecurityUtils.getSubject().getPrincipals().asList().size());
+    }
+
+    @Test
+    public void testAtLeastOneSuccessfulStrategy() throws Exception {
+        doLogin("classpath:shiro.authenticator.atLeastOne.success.ini", "mapros", "984138");
+        Assert.assertEquals(2,SecurityUtils.getSubject().getPrincipals().asList().size());
+    }
+
+    private void doLogin(String ini, String username, String password) {
+        Factory<SecurityManager> factory = new IniSecurityManagerFactory(ini);
+        SecurityManager securityManager = factory.getInstance();
+        SecurityUtils.setSecurityManager(securityManager);
+        Subject subject = SecurityUtils.getSubject();
+        UsernamePasswordToken token = new UsernamePasswordToken(username, password);
+        subject.login(token);
+    }
+}
---
+++ shiro-example/src/test/java/org/mapros/shiro/example/realm/AnotherCustomizedRealm.java
@@ -0,0 +1,31 @@
+package org.mapros.shiro.example.realm;
+
+import org.apache.shiro.authc.*;
+import org.apache.shiro.realm.Realm;
+
+/**
+ * Created by HP on 2016/10/10.
+ *
+ * @author mapros
+ */
+public class AnotherCustomizedRealm implements Realm {
+    public String getName() {
+        return getClass().getSimpleName();
+    }
+
+    public boolean supports(AuthenticationToken token) {
+        return token instanceof UsernamePasswordToken;
+    }
+
+    public AuthenticationInfo getAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+        String username = (String) token.getPrincipal();
+        String password = new String((char[]) token.getCredentials());
+        if (!"mapros".equals(username)) {
+            throw new UnknownAccountException();
+        }
+        if (!"984138".equals(password)) {
+            throw new IncorrectCredentialsException();
+        }
+        return new SimpleAuthenticationInfo(username+"@sina.com", password, getName());
+    }
+}
