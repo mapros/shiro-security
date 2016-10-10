f7b44f3ac8db262b739300c17b69897988c25ca0
mapros <mapros@sina.com>
2016-10-10 16:03:21 +0800

    jdbc realm ddl and ini configuration

--- shiro-example/src/test/java/org/mapros/shiro/example/LoginLogoutTest.java
+++ shiro-example/src/test/java/org/mapros/shiro/example/LoginLogoutTest.java
@@ -53,15 +53,15 @@ public class LoginLogoutTest {
         try {
             subject.login(token);
         } catch (UnknownAccountException uae) {
-            System.out.println("[ This is a unknown account ! ]");
+            System.out.println("[ This is a unknown account ]");
         } catch (IncorrectCredentialsException ice) {
-            System.out.println("[ Error password ! ]");
+            System.out.println("[ Error password ]");
         }
         //7.断言
         Assert.assertEquals(true, subject.isAuthenticated());
         //8.断言成功后退出登录
         subject.logout();
-        System.out.println("[ Log out success ! ]");
+        System.out.println("[ Log out success ]");
     }
 
     @Test
@@ -80,14 +80,41 @@ public class LoginLogoutTest {
         try {
             subject.login(token);
         } catch (UnknownAccountException uae) {
-            System.out.println("[ This is a unknown account ! ]");
+            System.out.println("[ This is a unknown account ]");
         } catch (IncorrectCredentialsException ice) {
-            System.out.println("[ Error password ! ]");
+            System.out.println("[ Error password ]");
         }
         //7.断言
         Assert.assertEquals(true, subject.isAuthenticated());
         //8.断言成功后退出登录
         subject.logout();
-        System.out.println("[ Log out success ! ]");
+        System.out.println("[ Log out success ]");
+    }
+
+    @Test
+    public void testJdbcRealm() throws Exception {
+        //1.使用ini文件创建SecurityManager工厂
+        Factory<SecurityManager> factory = new IniSecurityManagerFactory("classpath:shiro.jdbc.realm.ini");
+        //2.通过工厂获取SecurityManager实例
+        SecurityManager securityManager = factory.getInstance();
+        //3.将获取到的securityManager实例绑定到SecurityUtils
+        SecurityUtils.setSecurityManager(securityManager);
+        //4.获取Subject实例
+        Subject subject = SecurityUtils.getSubject();
+        //5.创建UsernamePasswordToken
+        UsernamePasswordToken token = new UsernamePasswordToken("mapros", "984138");
+        //6.登录认证
+        try {
+            subject.login(token);
+        } catch (UnknownAccountException uae) {
+            System.out.println("[ This is a unknown account ]");
+        } catch (IncorrectCredentialsException ice) {
+            System.out.println("[ Error password ]");
+        }
+        //7.断言
+        Assert.assertEquals(true, subject.isAuthenticated());
+        //8.断言成功后退出登录
+        subject.logout();
+        System.out.println("[ Log out success ]");
     }
 }
