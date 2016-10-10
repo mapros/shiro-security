cac500b9b6970d849063915fcddfdf6e1feaa924
mapros <mapros@sina.com>
2016-10-10 14:57:03 +0800

    add comment for LoginLogoutTest.java

--- shiro-example/src/test/java/org/mapros/shiro/example/LoginLogoutTest.java
+++ shiro-example/src/test/java/org/mapros/shiro/example/LoginLogoutTest.java
@@ -17,13 +17,21 @@ import org.junit.Test;
 public class LoginLogoutTest {
     @Test
     public void testLoginLogout() throws Exception {
+        //1.使用ini配置文件创建SecurityManager工厂
         Factory<SecurityManager> factory = new IniSecurityManagerFactory("classpath:shiro.ini");
+        //2.通过工厂获取一个SecurityManager的实例
         SecurityManager securityManager = factory.getInstance();
+        //3.将获取到的SecurityManager实例绑定到SecurityUtils
         SecurityUtils.setSecurityManager(securityManager);
+        //4.获取subject实例
         Subject subject = SecurityUtils.getSubject();
+        //5.创建一个UsernamePasswordToken
         UsernamePasswordToken token = new UsernamePasswordToken("mapros", "984138");
+        //6.将token交给subject进行登录认证
         subject.login(token);
+        //7.断言用户已经登录
         Assert.assertEquals(true, subject.isAuthenticated());
+        //8.退出
         subject.logout();
     }
 }
