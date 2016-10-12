1043723b35f76e80a300597950a45d18616e896f
mapros <mapros@sina.com>
2016-10-12 15:09:09 +0800

    shiro.jdbc.authorier.ini

--- shiro-authority/src/test/java/org/mapros/shiro/authority/AuthorizerTest.java
+++ shiro-authority/src/test/java/org/mapros/shiro/authority/AuthorizerTest.java
@@ -20,4 +20,16 @@ public class AuthorizerTest extends BaseTest {
         Assert.assertFalse(subject().isPermitted("+user1+4"));
         Assert.assertTrue(subject().isPermitted("menu:view"));
     }
+
+    @Test
+    public void testIsPermitted2() {
+        doLogin("classpath:shiro.jdbc.authorizer.ini", "mapros", "984138");
+        Assert.assertTrue(subject().isPermitted("user1:update"));
+        Assert.assertTrue(subject().isPermitted("user2:update"));
+        Assert.assertTrue(subject().isPermitted("+user1+2"));
+        Assert.assertTrue(subject().isPermitted("+user1+8"));
+        Assert.assertTrue(subject().isPermitted("+user2+10"));
+        Assert.assertFalse(subject().isPermitted("+user1+4"));
+        Assert.assertTrue(subject().isPermitted("menu:view"));
+    }
 }
